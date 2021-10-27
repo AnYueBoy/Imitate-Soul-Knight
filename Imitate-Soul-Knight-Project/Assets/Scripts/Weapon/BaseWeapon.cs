755773e6633db122627aeba52f61ce7ee51645ca
anyue <1183767577@qq.com>
2021-10-27 08:58:35 +0800

    临时提交

--- Imitate-Soul-Knight-Project/Assets/Scripts/Weapon/BaseWeapon.cs
+++ Imitate-Soul-Knight-Project/Assets/Scripts/Weapon/BaseWeapon.cs
@@ -14,6 +14,9 @@ public class BaseWeapon : MonoBehaviour {
     [SerializeField]
     protected int id;
 
+    [SerializeField]
+    protected SpriteRenderer shotGunEffect;
+
     protected float launchInterval = 0;
 
     protected float launchTimer = 0;
@@ -22,22 +25,44 @@ public class BaseWeapon : MonoBehaviour {
 
     protected string weaponTag;
 
+    protected float shotGunTimer = 0;
+
     public virtual void init (string tag) {
         this.weaponConfigData = ModuleManager.instance.configManager.weaponConfig.getWeaponConfigDataById (this.id);
         this.launchInterval = weaponConfigData.launchInterval;
         this.launchTimer = this.launchInterval;
         this.weaponTag = tag;
+        this.shotGunEffect.gameObject.SetActive (false);
     }
 
     public virtual void localUpdate (float dt) {
         this.launchTimer += dt;
+
+        this.refreshShotGun (dt);
+    }
+
+    protected void refreshShotGun (float dt) {
+        if (!this.shotGunEffect.gameObject.activeSelf) {
+            return;
+        }
+
+        this.shotGunTimer += dt;
+        this.shotGunEffect.color = new Color (1, 1, 1, (1 / this.launchInterval / 2) * this.shotGunTimer);
+        if (this.shotGunTimer > this.launchInterval / 3) {
+            this.shotGunEffect.gameObject.SetActive (false);
+            this.shotGunTimer = 0;
+        }
     }
 
     public virtual void launchBullet (float bulletDir) {
         if (this.launchTimer < this.launchInterval) {
             return;
         }
+
         this.launchTimer = 0;
         ModuleManager.instance.bulletManager.spawnBullet (launchTrans, bulletDir, this.weaponTag, this.weaponConfigData);
+
+        this.shotGunEffect.gameObject.SetActive (true);
+        this.shotGunEffect.color = new Color (1, 1, 1, 1);
     }
 }
