c700ce6a4178ec4886b0c0d9afa5d4b5d39d95a5
anyue <1183767577@qq.com>
2021-10-27 15:37:56 +0800

    添加枪械后坐力逻辑

--- Imitate-Soul-Knight-Project/Assets/Scripts/Weapon/BaseWeapon.cs
+++ Imitate-Soul-Knight-Project/Assets/Scripts/Weapon/BaseWeapon.cs
@@ -17,18 +17,32 @@ public class BaseWeapon : MonoBehaviour {
     [SerializeField]
     protected SpriteRenderer shotGunEffect;
 
-    protected float launchInterval = 0;
-
-    protected float launchTimer = 0;
-
     protected WeaponConfigData weaponConfigData;
 
     protected string weaponTag;
 
+    #region  枪口火焰
     protected float shotGunEffectInterval;
 
     protected float shotGunEffectSpeed;
     protected float shotGunTimer = 0;
+
+    #endregion
+
+    #region  发射
+    protected float launchInterval = 0;
+
+    protected float launchTimer = 0;
+
+    #endregion
+
+    #region  后坐力
+    protected readonly float recoilForeceDis = 0.012f;
+    protected float recoilForceTimer = 0;
+    protected float recoilForceSpeed;
+    protected float recoilForceInterval;
+    protected bool isRecoilForce = false;
+    #endregion
 
     public virtual void init (string tag) {
         this.weaponConfigData = ModuleManager.instance.configManager.weaponConfig.getWeaponConfigDataById (this.id);
@@ -39,12 +53,17 @@ public class BaseWeapon : MonoBehaviour {
 
         this.shotGunEffectInterval = this.launchInterval / 2;
         this.shotGunEffectSpeed = 1 / shotGunEffectInterval;
+
+        this.recoilForceInterval = (this.launchInterval / 2) / 2;
+        this.recoilForceSpeed = this.recoilForeceDis / this.recoilForceInterval;
     }
 
     public virtual void localUpdate (float dt) {
         this.launchTimer += dt;
 
         this.refreshShotGun (dt);
+
+        this.recoilForce(dt);
     }
 
     protected void refreshShotGun (float dt) {
@@ -70,5 +89,25 @@ public class BaseWeapon : MonoBehaviour {
 
         this.shotGunEffect.gameObject.SetActive (true);
         this.shotGunEffect.color = new Color (1, 1, 1, 1);
+
+        this.isRecoilForce = true;
+    }
+
+    protected virtual void recoilForce (float dt) {
+        if (!this.isRecoilForce) {
+            return;
+        }
+        this.recoilForceTimer += dt;
+        if (this.recoilForceTimer > this.recoilForceInterval) {
+            this.recoilForceTimer = 0;
+            if (this.recoilForceSpeed < 0) {
+                this.transform.localPosition = Vector3.zero;
+                this.isRecoilForce = false;
+            }
+            this.recoilForceSpeed = -this.recoilForceSpeed;
+        }
+        float nodeX = this.transform.localPosition.x;
+        this.transform.localPosition = new Vector3 (nodeX - this.recoilForceTimer * this.recoilForceSpeed, 0, 0);
+
     }
 }
