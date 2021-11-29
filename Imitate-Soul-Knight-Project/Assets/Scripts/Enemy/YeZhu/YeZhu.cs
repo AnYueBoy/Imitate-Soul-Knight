0cc4d75cee45839f4f46de7a884ff8a1c9fc6893
anyue <1183767577@qq.com>
2021-11-29 18:31:52 +0800

    格式化代码

--- Imitate-Soul-Knight-Project/Assets/Scripts/Enemy/YeZhu/YeZhu.cs
+++ Imitate-Soul-Knight-Project/Assets/Scripts/Enemy/YeZhu/YeZhu.cs
@@ -1,10 +1,10 @@
-using System.Collections.Generic;
 /*
  * @Author: l hy 
  * @Date: 2021-10-28 17:25:31 
  * @Description: 野猪怪
  */
 
+using System.Collections.Generic;
 using UFramework;
 using UFramework.AI.BehaviourTree.Node;
 using UFramework.AI.BlackBoard;
