9e75670d7c4f203369a522581baae34d5ca1c992
anyue <1183767577@qq.com>
2021-12-08 18:30:18 +0800

    添加命名空间

--- Imitate-Soul-Knight-Project/Assets/UFramework/Scripts/Tween/Tweener.cs
+++ Imitate-Soul-Knight-Project/Assets/UFramework/Scripts/Tween/Tweener.cs
@@ -3,7 +3,8 @@
  * @Date: 2021-12-08 18:24:38 
  * @Description: Tweener
  */
-
-public class Tweener<T> {
-    public T target;
+namespace UFramework.Tween {
+    public class Tweener<T> {
+        public T target;
+    }
 }
