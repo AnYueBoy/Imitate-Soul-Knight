4bbc2c859d67d9eff4e377ef31d9517d65c51353
anyue <1183767577@qq.com>
2021-12-18 17:25:25 +0800

    临时提交

--- Imitate-Soul-Knight-Project/Assets/Scripts/Weapon/MeleeWeapon.cs
+++ Imitate-Soul-Knight-Project/Assets/Scripts/Weapon/MeleeWeapon.cs
@@ -5,8 +5,12 @@
  */
 
 using UFramework;
+using UFramework.GameCommon;
+using UnityEngine;
 
 public class MeleeWeapon : BaseWeapon {
+    private Animator animator;
+
     public override void attack (float args) {
         if (this.attackTimer < this.attackInterval) {
             return;
@@ -22,11 +26,47 @@ public class MeleeWeapon : BaseWeapon {
 
         this.attackTimer = 0;
 
+        this.checkEffect ();
+        this.playAttackAnimation ();
+
         // TODO: 攻击伤害逻辑
 
     }
 
+    private void checkEffect () {
+        if (this.animator != null) {
+            return;
+        }
+        GameObject effectPrefab = AssetsManager.instance.getAssetByUrlSync<GameObject> (this.weaponConfigData.bulletUrl);
+        GameObject effectNode = ObjectPool.instance.requestInstance (effectPrefab);
+        this.animator = effectNode.GetComponent<Animator> ();
+    }
+
+    private bool isInAttackState = false;
+
+    private void playAttackAnimation () {
+        this.animator.SetTrigger ("Attack");
+    }
+
     public override void localUpdate (float dt) {
         this.attackTimer += dt;
+        this.checkAttackState ();
+        this.checkRayCast ();
+    }
+
+    private void checkAttackState () {
+        if (!this.isInAttackState) {
+            return;
+        }
+
+        AnimatorStateInfo info = this.animator.GetCurrentAnimatorStateInfo (0);
+        this.isInAttackState = info.IsName ("blueSword");
+    }
+
+    private void checkRayCast () {
+        if (!this.isInAttackState) {
+            return;
+        }
+        //TODO: box ray check
     }
 }
