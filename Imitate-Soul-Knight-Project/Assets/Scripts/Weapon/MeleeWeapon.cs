88b8479212d1246953b1fdf2eceff00a23a33364
anyue <1183767577@qq.com>
2021-12-21 09:39:12 +0800

    调整动画控制器

--- Imitate-Soul-Knight-Project/Assets/Scripts/Weapon/MeleeWeapon.cs
+++ Imitate-Soul-Knight-Project/Assets/Scripts/Weapon/MeleeWeapon.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 /*
  * @Author: l hy 
  * @Date: 2021-12-16 13:31:08 
@@ -12,13 +11,20 @@ using UnityEngine;
 
 public class MeleeWeapon : BaseWeapon {
 
-    private List<Sprite> effectSpriteList;
+    [SerializeField] protected Animator animator;
+
+    protected float attackAnimationTime;
 
     public override void init (ItemIdEnum id) {
         base.init (id);
-
-        // 加载近战武器的效果图片
-        effectSpriteList = AssetsManager.instance.getAllAssetsByUrlSync<Sprite> ("Effect/" + (int) id);
+        AnimationClip[] animationClips = this.animator.runtimeAnimatorController.animationClips;
+        for (int i = 0; i < animationClips.Length; i++) {
+            AnimationClip clip = animationClips[i];
+            if (clip.name == "attack") {
+                this.attackAnimationTime = clip.length;
+                break;
+            }
+        }
     }
 
     public override void attack (float args) {
@@ -39,63 +45,13 @@ public class MeleeWeapon : BaseWeapon {
         ModuleManager.instance.playerManager.consumeMp (weaponConsumeValue);
 
         this.attackTimer = 0;
-
-        this.playAttackAnimation ();
     }
 
     private bool isInAttackState = false;
-    private readonly float attackAnimationTime = 0.2f;
-
-    private float animationSpeed;
-
-    private float animationTimer;
-
-    private readonly float leftAngle = 66;
-
-    private readonly float rightAngle = -66;
-
-    private void playAttackAnimation () {
-
-        // 开启动画
-        this.animationSpeed = (leftAngle - rightAngle) / this.attackAnimationTime;
-        this.isInAttackState = true;
-        this.animationTimer = 0;
-
-        Transform meleeParentTransform = ModuleManager.instance.playerManager.getMeleeParentTransform ();
-        meleeParentTransform.position = new Vector3 (0.638f, 0, 0);
-    }
 
     public override void localUpdate (float dt) {
         this.attackTimer += dt;
-        // this.executeAnimation (dt);
         this.checkRayCast ();
-    }
-
-    private void executeAnimation (float dt) {
-        if (!this.isInAttackState) {
-            return;
-        }
-
-        this.animationTimer += dt;
-
-        Transform meleeRotationTransfrom = ModuleManager.instance.playerManager.getMeleeRotationTransform ();
-
-        if (this.animationTimer > this.attackAnimationTime) {
-            this.animationTimer = 0;
-            this.isInAttackState = false;
-            meleeRotationTransfrom.localEulerAngles = new Vector3 (0, 0, 30);
-            return;
-        }
-
-        SpriteRenderer effectSpriteRender = ModuleManager.instance.playerManager.getEffectSprite ();
-        if (meleeRotationTransfrom.localEulerAngles.z <= -34 && effectSpriteRender.sprite != this.effectSpriteList[0]) {
-            // 显示第一帧刀光
-            effectSpriteRender.sprite = this.effectSpriteList[0];
-        }
-
-        float zEulerAngles = 66 - this.attackTimer * this.animationSpeed;
-
-        meleeRotationTransfrom.localEulerAngles = new Vector3 (0, 0, zEulerAngles);
     }
 
     private void checkRayCast () {
