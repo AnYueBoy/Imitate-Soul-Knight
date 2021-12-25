cdd2753365c372f8907a6560450e02926e28f39e
anyue <1183767577@qq.com>
2021-12-25 16:31:42 +0800

    添加行为树编辑器的临时逻辑

---
+++ Imitate-Soul-Knight-Project/Assets/Editor/Behaviour/BehaviourTreeMonitor.cs
@@ -0,0 +1,39 @@
+﻿using UFramework.AI.BehaviourTree;
+using UnityEditor;
+using UnityEngine;
+
+public class BehaviourTreeMonitor : EditorWindow {
+
+    [MenuItem ("UFramework/BehaviourTreeMonitor")]
+    private static void ShowWindow () {
+        var window = GetWindow<BehaviourTreeMonitor> ();
+        window.titleContent = new GUIContent ("BehaviourTreeMonitor");
+        window.Show ();
+    }
+
+    private Texture lightTexture;
+
+    private void Awake () {
+        this.lightTexture = Resources.Load<Texture> ("BehaviourTree/LightTaskBlue");
+    }
+
+    private void OnGUI () {
+        this.drawBehaviourTree ();
+    }
+
+    private void drawBehaviourTree () {
+        GameObject behaviourNode = Selection.activeGameObject;
+        if (behaviourNode == null) {
+            return;
+        }
+
+        BehaviourTreeRunner behaviourTreeRunner = behaviourNode.GetComponent<BehaviourTreeRunner> ();
+        if (behaviourTreeRunner == null) {
+            return;
+        }
+
+        // TODO: 构建行为树
+        EditorGUILayout.ObjectField ("添加贴图", lightTexture, typeof (Texture), true);
+        EditorGUILayout.LabelField ("选择item: " + behaviourNode.name);
+    }
+}
