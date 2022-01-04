6ac0206ab389440a5328256c5b8bd47fc9a7b211
anyue <1183767577@qq.com>
2022-01-04 16:36:04 +0800

    添加选择逻辑

--- Imitate-Soul-Knight-Project/Assets/Editor/Behaviour/BehaviourTreeMonitor.cs
+++ Imitate-Soul-Knight-Project/Assets/Editor/Behaviour/BehaviourTreeMonitor.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class BehaviourTreeMonitor : EditorWindow {
     private List<Node> nodes;
+    private List<Connection> connections;
     private GUIStyle nodeStyle;
+    private GUIStyle inPointStyle;
+    private GUIStyle outPointStyle;
+    private ConnectionPoint selectedInPoint;
+    private ConnectionPoint selectedOutPoint;
 
     [MenuItem ("UFramework/BehaviourTreeMonitor")]
     private static void ShowWindow () {
@@ -19,27 +24,121 @@ public class BehaviourTreeMonitor : EditorWindow {
         Texture2D texture = EditorGUIUtility.Load ("builtin skins/darkskin/images/node1.png") as Texture2D;
         nodeStyle.normal.background = texture;
         nodeStyle.border = new RectOffset (12, 12, 12, 12);
+
+        inPointStyle = new GUIStyle ();
+        inPointStyle.normal.background = EditorGUIUtility.Load ("builtin skins/darkskin/images/btn left.png") as Texture2D;
+        inPointStyle.active.background = EditorGUIUtility.Load ("builtin skins/darkskin/images/btn left on.png") as Texture2D;
+        inPointStyle.border = new RectOffset (4, 4, 12, 12);
+
+        outPointStyle = new GUIStyle ();
+        outPointStyle.normal.background = EditorGUIUtility.Load ("builtin skins/darkskin/images/btn right.png") as Texture2D;
+        outPointStyle.active.background = EditorGUIUtility.Load ("builtin skins/darkskin/images/btn right on.png") as Texture2D;
+        outPointStyle.border = new RectOffset (4, 4, 12, 12);
     }
 
     private void OnGUI () {
         this.drawBehaviourTree ();
         this.drawNodes ();
-        this.processNodesEvent (Event.current);
-        this.processEvent (Event.current);
+        this.drawConnection ();
+        this.processNodesEvents (Event.current);
+        this.processEvents (Event.current);
         if (GUI.changed) Repaint ();
     }
 
-    private void processEvent (Event e) {
-        switch (e.type) {
-            case EventType.MouseDown:
-                if (e.button == 1) {
-                    this.processContextMenu (e.mousePosition);
-                }
-                break;
-            default:
-                break;
+    private void drawNodes () {
+        if (this.nodes == null) {
+            return;
         }
 
+        for (int i = 0; i < nodes.Count; i++) {
+            nodes[i].draw ();
+        }
+    }
+
+    private void drawConnection () {
+        if (connections == null) {
+            return;
+        }
+
+        for (int i = 0; i < connections.Count; i++) {
+            connections[i].draw ();
+        }
+    }
+
+    private void processEvents (Event e) {
+        if (e.type != EventType.MouseDown) {
+            return;
+        }
+        if (e.button == 0) {
+            clearConnectionSelection ();
+        }
+        if (e.button == 1) {
+            this.processContextMenu (e.mousePosition);
+        }
+    }
+
+    private void processNodesEvents (Event e) {
+        if (nodes == null) {
+            return;
+        }
+
+        for (int i = nodes.Count - 1; i >= 0; i--) {
+            bool guiChanged = nodes[i].processEvents (e);
+            if (guiChanged) {
+                GUI.changed = true;
+            }
+        }
+    }
+
+    private void processContextMenu (Vector2 mousePosition) {
+        GenericMenu genericMenu = new GenericMenu ();
+        genericMenu.AddItem (new GUIContent ("Add Item"), false, () => onClickAddNode (mousePosition));
+        genericMenu.ShowAsContext ();
+    }
+
+    private void onClickAddNode (Vector2 mousePosition) {
+        if (nodes == null) {
+            nodes = new List<Node> ();
+        }
+        nodes.Add (new Node (mousePosition, 100, 120, nodeStyle, inPointStyle, outPointStyle, onClickInPoint, onClickOutPoint));
+    }
+
+    private void onClickInPoint (ConnectionPoint inPoint) {
+        selectedInPoint = inPoint;
+        if (selectedOutPoint == null) {
+            return;
+        }
+
+        if (selectedOutPoint.node != selectedInPoint.node) {
+            this.createConnection ();
+        }
+        this.clearConnectionSelection ();
+    }
+    private void onClickOutPoint (ConnectionPoint outPoint) {
+        selectedOutPoint = outPoint;
+        if (selectedInPoint == null) {
+            return;
+        }
+        if (selectedOutPoint.node != selectedInPoint.node) {
+            this.createConnection ();
+        }
+        this.clearConnectionSelection ();
+    }
+
+    private void onClickRemoveConnection (Connection connection) {
+        connections.Remove (connection);
+    }
+
+    private void createConnection () {
+        if (connections == null) {
+            connections = new List<Connection> ();
+        }
+
+        connections.Add (new Connection (selectedInPoint, selectedOutPoint, onClickRemoveConnection));
+    }
+
+    private void clearConnectionSelection () {
+        selectedInPoint = selectedOutPoint = null;
     }
 
     private void drawBehaviourTree () {
@@ -54,40 +153,5 @@ public class BehaviourTreeMonitor : EditorWindow {
         }
 
         // this.drawNodes ();
-    }
-
-    private void drawNodes () {
-        if (this.nodes != null) {
-            foreach (Node node in nodes) {
-                node.draw ();
-            }
-        }
-    }
-
-    private void processContextMenu (Vector2 mousePosition) {
-        GenericMenu genericMenu = new GenericMenu ();
-        genericMenu.AddItem (new GUIContent ("Add Item"), false, () => onClickAddNode (mousePosition));
-        genericMenu.ShowAsContext ();
-    }
-
-    private void processNodesEvent (Event e) {
-        if (nodes == null) {
-            return;
-        }
-
-        for (int i = nodes.Count - 1; i >= 0; i--) {
-            bool guiChanged = nodes[i].processEvents (e);
-            if (guiChanged) {
-                GUI.changed = true;
-            }
-        }
-    }
-
-    private void onClickAddNode (Vector2 mousePosition) {
-        if (nodes == null) {
-            nodes = new List<Node> ();
-        }
-
-        nodes.Add (new Node (mousePosition, 100, 120, nodeStyle));
     }
 }
--- Imitate-Soul-Knight-Project/Assets/Editor/Behaviour/Node.cs
+++ Imitate-Soul-Knight-Project/Assets/Editor/Behaviour/Node.cs
@@ -1,3 +1,4 @@
+using System;
 /*
  * @Author: l hy 
  * @Date: 2022-01-04 10:03:17 
@@ -9,21 +10,33 @@ using UnityEngine;
 public class Node {
 
     public Rect rect;
-
-    public string title = "默认";
-    public bool isDragged = false;
-
+    public string title;
+    public bool isDragged;
+    public ConnectionPoint inPoint;
+    public ConnectionPoint outPoint;
     public GUIStyle style;
 
-    public Node (Vector2 postion, float width, float height, GUIStyle style) {
+    public Node (
+        Vector2 postion,
+        float width,
+        float height,
+        GUIStyle nodeStyle,
+        GUIStyle inPointStyle,
+        GUIStyle outPointStyle,
+        Action<ConnectionPoint> onClickInPoint,
+        Action<ConnectionPoint> onClickOutPoint) {
         rect = new Rect (postion.x, postion.y, width, height);
-        this.style = style;
+        this.style = nodeStyle;
+        this.inPoint = new ConnectionPoint (this, ConnectionPointType.In, inPointStyle, onClickOutPoint);
+        this.outPoint = new ConnectionPoint (this, ConnectionPointType.Out, outPointStyle, onClickOutPoint);
     }
 
     public void drag (Vector2 delta) {
         rect.position += delta;
     }
     public void draw () {
+        inPoint.draw ();
+        outPoint.draw ();
         GUI.Box (rect, title, style);
     }
 
