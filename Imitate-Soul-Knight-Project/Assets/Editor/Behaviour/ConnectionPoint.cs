79ebb22635b90cfd6fbc9df49fbfd27b04c8397a
anyue <1183767577@qq.com>
2022-01-05 12:08:53 +0800

    调整输入输出节点大小

--- Imitate-Soul-Knight-Project/Assets/Editor/Behaviour/ConnectionPoint.cs
+++ Imitate-Soul-Knight-Project/Assets/Editor/Behaviour/ConnectionPoint.cs
@@ -19,20 +19,20 @@ public class ConnectionPoint {
         this.type = type;
         this.style = style;
         this.onClickConnectionPoint = onClickConnectionPoint;
-        this.rect = new Rect (0, 0, 10f, 20f);
+        this.rect = new Rect (0, 0, 20f, 10f);
     }
 
     public void draw () {
-        // GUI坐标左下角
-        rect.y = node.rect.y + node.rect.height / 2 - rect.height / 2;
+        // GUI坐标左上角为（0,0），右下角为(screenWidth,screenHeight)
+        rect.x = node.rect.x + node.rect.width / 2 - rect.width / 2;
 
         switch (type) {
             case ConnectionPointType.In:
-                rect.x = node.rect.x - rect.width + 8f;
+                rect.y = node.rect.y - rect.height / 2;
                 break;
 
             case ConnectionPointType.Out:
-                rect.x = node.rect.x + node.rect.width - 8f;
+                rect.y = node.rect.y + node.rect.height - rect.height;
                 break;
             default:
                 break;
